eaed61f24184508c1536f03df660db374b1a3f0f
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2024-06-29 21:37:04 +0900

    Add keyboard input handling for discarding cards

--- Assets/MahCard/Scripts/AI/Input.cs
+++ Assets/MahCard/Scripts/AI/Input.cs
@@ -1,5 +1,6 @@
 using System.Threading;
 using Cysharp.Threading.Tasks;
+using R3;
 using UnityEngine.InputSystem;
 
 namespace MahCard.AI
@@ -12,6 +13,30 @@ namespace MahCard.AI
         public UniTask<int> DiscardAsync(CancellationToken scope)
         {
             var source = new UniTaskCompletionSource<int>();
+            Observable.EveryUpdate(scope)
+                .Subscribe(_ =>
+                {
+                    if (Keyboard.current.digit1Key.wasPressedThisFrame)
+                    {
+                        source.TrySetResult(0);
+                    }
+                    else if (Keyboard.current.digit2Key.wasPressedThisFrame)
+                    {
+                        source.TrySetResult(1);
+                    }
+                    else if (Keyboard.current.digit3Key.wasPressedThisFrame)
+                    {
+                        source.TrySetResult(2);
+                    }
+                    else if (Keyboard.current.digit4Key.wasPressedThisFrame)
+                    {
+                        source.TrySetResult(3);
+                    }
+                    else if (Keyboard.current.digit5Key.wasPressedThisFrame)
+                    {
+                        source.TrySetResult(4);
+                    }
+                });
             return source.Task;
         }
     }
