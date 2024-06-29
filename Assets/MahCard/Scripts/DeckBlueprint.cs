c80a0fdcc4a2419fcdb817c1793033270c24c4d2
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2024-06-29 20:27:04 +0900

    Refactor CreateDeck method in DeckBlueprint.cs

--- Assets/MahCard/Scripts/DeckBlueprint.cs
+++ Assets/MahCard/Scripts/DeckBlueprint.cs
@@ -11,6 +11,18 @@ namespace MahCard
     {
         [SerializeField]
         private List<CardBlueprint> cards;
-        public List<CardBlueprint> Cards => cards;
+
+        public Deck CreateDeck()
+        {
+            var deck = new Deck();
+            foreach (var card in cards)
+            {
+                foreach (var c in card.CreateCards())
+                {
+                    deck.Push(c);
+                }
+            }
+            return deck;
+        }
     }
 }
