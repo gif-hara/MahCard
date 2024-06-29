9ec8079bc4b2c7f4fdb1f5038083d0c719b8b30f
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2024-06-29 19:45:23 +0900

    Add game state machine and async initialization

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -1,4 +1,7 @@
 using System.Collections.Generic;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using TinyStateMachineSystems;
 
 namespace MahCard
 {
@@ -12,5 +15,30 @@ namespace MahCard
         public Deck Deck { get; }
 
         public Deck DiscardDeck { get; }
+
+        public GameRules Rules { get; }
+
+        private readonly TinyStateMachine stateMachine = new();
+
+        public Game(IEnumerable<User> users, Deck deck, Deck discardDeck, GameRules rules)
+        {
+            Users = new List<User>(users);
+            Deck = deck;
+            DiscardDeck = discardDeck;
+            Rules = rules;
+        }
+
+        private UniTask StateGameStart(CancellationToken scope)
+        {
+            Deck.Shuffle();
+            foreach (var user in Users)
+            {
+                for (var i = 0; i < Rules.HandCardCount; i++)
+                {
+                    user.Draw(Deck);
+                }
+            }
+            return UniTask.CompletedTask;
+        }
     }
 }
