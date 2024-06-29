c47d1bb6a06b5c2411f2661582a047716df83a3a
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2024-06-29 20:09:43 +0900

    Add DeckBlueprint property to GameRules class

--- Assets/MahCard/Scripts/GameRules.cs
+++ Assets/MahCard/Scripts/GameRules.cs
@@ -5,10 +5,15 @@ namespace MahCard
     /// <summary>
     /// 
     /// </summary>
+    [CreateAssetMenu(fileName = "GameRules", menuName = "MahCard/GameRules")]
     public sealed class GameRules : ScriptableObject
     {
         [SerializeField]
         private int handCardCount;
         public int HandCardCount => handCardCount;
+
+        [SerializeField]
+        private DeckBlueprint deckBlueprint;
+        public DeckBlueprint DeckBlueprint => deckBlueprint;
     }
 }
