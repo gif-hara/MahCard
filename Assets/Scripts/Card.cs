f42818b8bee617be1cb50a9c244868dd2017479f
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-06-29 18:00:47 +0900

    feat: Add Card class and Define enums for MahCard game

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,19 @@
+
+namespace MahCard
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public sealed class Card
+    {
+        public Define.CardColor Color { get; }
+
+        public Define.CardAbility Ability { get; }
+
+        public Card(Define.CardColor color, Define.CardAbility ability)
+        {
+            Color = color;
+            Ability = ability;
+        }
+    }
+}
---
+++ Assets/Scripts/Define.cs
@@ -0,0 +1,22 @@
+namespace MahCard
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class Define
+    {
+        public enum CardColor
+        {
+            Red,
+            Blue,
+            Green,
+            White,
+            Black
+        }
+
+        public enum CardAbility
+        {
+            None,
+        }
+    }
+}
