43120bcd1041b5df5d97205e7cf864f52affadae
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-06-30 18:04:44 +0900

    feat: Add Log view class for MahCard game

---
+++ Assets/MahCard/Scripts/View/Log.cs
@@ -0,0 +1,15 @@
+using Cysharp.Threading.Tasks;
+
+namespace MahCard.View
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public sealed class Log : IView
+    {
+        public UniTask OnGameStartAsync()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
