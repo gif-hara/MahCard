7a7d49e765724aa98f13d52f92ff88e8633b4f74
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-07-02 01:22:46 +0900

    feat: Add keyboard input for choosing deck type in ChoiceDeckTypeAsync method

--- Assets/MahCard/Scripts/AI/Input.cs
+++ Assets/MahCard/Scripts/AI/Input.cs
@@ -1,6 +1,7 @@
 using System.Threading;
 using Cysharp.Threading.Tasks;
 using R3;
+using UnityEngine;
 using UnityEngine.InputSystem;
 
 namespace MahCard.AI
@@ -36,7 +37,8 @@ namespace MahCard.AI
                     {
                         source.TrySetResult(4);
                     }
-                });
+                })
+                .RegisterTo(scope);
             user.OnSelectedCardIndex
                 .Subscribe(x =>
                 {
@@ -49,6 +51,19 @@ namespace MahCard.AI
         public UniTask<Define.DeckType> ChoiceDeckTypeAsync(Game game, User user, CancellationToken scope)
         {
             var source = new UniTaskCompletionSource<Define.DeckType>();
+            Observable.EveryUpdate(scope)
+                .Subscribe(_ =>
+                {
+                    if (Keyboard.current.digit1Key.wasPressedThisFrame)
+                    {
+                        source.TrySetResult(Define.DeckType.Deck);
+                    }
+                    else if (Keyboard.current.digit2Key.wasPressedThisFrame)
+                    {
+                        source.TrySetResult(Define.DeckType.DiscardDeck);
+                    }
+                })
+                .RegisterTo(scope);
             user.OnSelectedDeckType
                 .Subscribe(x =>
                 {
