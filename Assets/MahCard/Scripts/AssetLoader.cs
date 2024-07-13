0dcee1366a01d821ae173b1828e6ef733cee1256
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-07-13 20:39:10 +0900

    feat: Add AssetLoader class for loading assets asynchronously

---
+++ Assets/MahCard/Scripts/AssetLoader.cs
@@ -0,0 +1,17 @@
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+namespace MahCard
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public sealed class AssetLoader
+    {
+        public async UniTask<T> LoadAsync<T>(string path) where T : UnityEngine.Object
+        {
+            var result = await Resources.LoadAsync<T>(path).ToUniTask();
+            return result as T;
+        }
+    }
+}
