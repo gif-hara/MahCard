505f763b9d2a842be0a76dceb9f38518ec5a008d
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-07-13 20:43:59 +0900

    refactor: Update AssetLoader.cs to use static class for loading assets asynchronously

--- Assets/MahCard/Scripts/AssetLoader.cs
+++ Assets/MahCard/Scripts/AssetLoader.cs
@@ -6,9 +6,9 @@ namespace MahCard
     /// <summary>
     /// 
     /// </summary>
-    public sealed class AssetLoader
+    public static class AssetLoader
     {
-        public async UniTask<T> LoadAsync<T>(string path) where T : Object
+        public static async UniTask<T> LoadAsync<T>(string path) where T : Object
         {
             var result = await Resources.LoadAsync<T>(path).ToUniTask();
             return result as T;
