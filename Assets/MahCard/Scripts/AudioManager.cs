1026af9d97767bc73f507c2e22a1bc97f9b142cf
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-07-14 09:11:25 +0900

    refactor: Update AudioManager.cs to use static methods for playing BGM and SFX

--- Assets/MahCard/Scripts/AudioManager.cs
+++ Assets/MahCard/Scripts/AudioManager.cs
@@ -1,3 +1,4 @@
+using HK;
 using UnityEngine;
 
 namespace MahCard
@@ -13,13 +14,23 @@ namespace MahCard
         [SerializeField]
         private AudioSource sfxSource;
 
-        public void PlayBGM(AudioClip clip)
+        public static void PlayBGM(AudioClip clip)
+        {
+            TinyServiceLocator.Resolve<AudioManager>().PlayBGMInternal(clip);
+        }
+
+        public static void PlaySFX(AudioClip clip)
+        {
+            TinyServiceLocator.Resolve<AudioManager>().PlaySFXInternal(clip);
+        }
+
+        private void PlayBGMInternal(AudioClip clip)
         {
             bgmSource.clip = clip;
             bgmSource.Play();
         }
 
-        public void PlaySFX(AudioClip clip)
+        private void PlaySFXInternal(AudioClip clip)
         {
             sfxSource.PlayOneShot(clip);
         }
