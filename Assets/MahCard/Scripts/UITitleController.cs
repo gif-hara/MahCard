7cd0da8a9440bd424197b505314bba083a9028ac
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2024-07-14 22:35:47 +0900

    feat: Add UITitleController script for MahCard game

---
+++ Assets/MahCard/Scripts/UITitleController.cs
@@ -0,0 +1,55 @@
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using HK;
+using R3;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace MahCard
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public sealed class UITitleController
+    {
+        private readonly HKUIDocument titleDocumentPrefab;
+
+        private readonly HKUIDocument guideBookDocumentPrefab;
+
+        public UITitleController(HKUIDocument document)
+        {
+            this.titleDocumentPrefab = document;
+        }
+
+        public void Open(CancellationToken scope)
+        {
+            var document = Object.Instantiate(this.titleDocumentPrefab);
+            var anyClickAreaDocument = document.Q<HKUIDocument>("Area.AnyClick");
+            var mainAreaDocument = document.Q<HKUIDocument>("Area.Main");
+
+            anyClickAreaDocument.Q<Button>("Button").OnClickAsObservable()
+                .Subscribe(_ =>
+                {
+                    mainAreaDocument.gameObject.SetActive(true);
+                    anyClickAreaDocument.gameObject.SetActive(false);
+                })
+                .RegisterTo(document.destroyCancellationToken);
+
+            mainAreaDocument.Q<Button>("Button.GameStart").OnClickAsObservable()
+                .Subscribe(_ =>
+                {
+                    SceneManager.LoadScene("Game");
+                })
+                .RegisterTo(document.destroyCancellationToken);
+
+            mainAreaDocument.Q<Button>("Button.GuideBook").OnClickAsObservable()
+                .Subscribe(_ =>
+                {
+                    var guideBookController = new UIGuideBookController(guideBookDocumentPrefab);
+                    guideBookController.OpenAsync(scope).Forget();
+                })
+                .RegisterTo(document.destroyCancellationToken);
+        }
+    }
+}
