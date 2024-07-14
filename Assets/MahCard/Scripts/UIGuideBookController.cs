86c9ae204cfe36e37711243d9396e0dc081ebf0e
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2024-07-14 18:40:52 +0900

    Add page navigation functionality to UIGuideBookController.cs

--- Assets/MahCard/Scripts/UIGuideBookController.cs
+++ Assets/MahCard/Scripts/UIGuideBookController.cs
@@ -1,6 +1,9 @@
+using System.Collections.Generic;
 using System.Threading;
 using Cysharp.Threading.Tasks;
 using HK;
+using R3;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -21,10 +24,52 @@ namespace MahCard
         public async UniTask OpenAsync(CancellationToken scope)
         {
             var document = Object.Instantiate(documentPrefab);
+            var pageAreaDocument = document.Q<HKUIDocument>("Area.Page");
+            var indexAreaDocument = document.Q<HKUIDocument>("Area.Index");
+            var nextButton = indexAreaDocument.Q<Button>("Button.Next");
+            var previousButton = indexAreaDocument.Q<Button>("Button.Previous");
+            var indexText = indexAreaDocument.Q<TMP_Text>("Text");
+            var pages = new List<GameObject>();
+            var currentPageIndex = 0;
+            while (true)
+            {
+                var page = pageAreaDocument.TryQ(currentPageIndex.ToString());
+                if (page == null)
+                {
+                    break;
+                }
+                pages.Add(page);
+                page.SetActive(false);
+                currentPageIndex++;
+            }
+            currentPageIndex = 0;
+            ChangePage(currentPageIndex);
+            nextButton.OnClickAsObservable()
+                .Subscribe(_ =>
+                {
+                    ChangePage(currentPageIndex + 1);
+                })
+                .RegisterTo(document.destroyCancellationToken);
+            previousButton.OnClickAsObservable()
+                .Subscribe(_ =>
+                {
+                    ChangePage(currentPageIndex - 1);
+                })
+                .RegisterTo(document.destroyCancellationToken);
 
             await document.Q<Button>("Button.Close").OnClickAsync(scope);
 
             Object.Destroy(document.gameObject);
+
+            void ChangePage(int nextPageIndex)
+            {
+                pages[currentPageIndex].SetActive(false);
+                pages[nextPageIndex].SetActive(true);
+                currentPageIndex = nextPageIndex;
+                nextButton.gameObject.SetActive(nextPageIndex < pages.Count - 1);
+                previousButton.gameObject.SetActive(nextPageIndex > 0);
+                indexText.text = $"{nextPageIndex + 1}/{pages.Count}";
+            }
         }
     }
 }
